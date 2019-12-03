94224295af5c436d1ba49f5282812384f15bf974
Valentinez <suejunhong@gmail.com>
2019-12-03 10:19:11 +0800

    Edited Commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,21 @@
 import React from "react"
+import { Link } from "gatsby";
+import { Banner, TextWrapper, MoreText } from "../styles/IndexStyles";
 
-export default () => <div>Hello world!</div>
+
+export default () => (
+    
+<div style={{position:'relative'}}>
+    <Banner></Banner>
+    <TextWrapper>
+        <div>
+            <h2>TravelPreneur</h2>
+            <p>One Stop For<br/>
+            All your Travel Needs<br/>
+            And Luxury Needs<br/></p>
+            <Link to="/works">Our Work</Link>       
+        </div>
+    </TextWrapper>
+    <MoreText>Learn More</MoreText>
+</div>
+)
